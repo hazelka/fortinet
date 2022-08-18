fde8d1d13be35b1450324584521f4075cc1266a2
Hazel Feng <hazel@Hazels-Air.hitronhub.home>
2022-08-18 03:06:42 -0700

    Fix bugs in task3.js

--- task3.js
+++ task3.js
@@ -7,23 +7,39 @@
 function maxMonkeysCanBeFed(apple, banana) {
   if (apple === 0 && banana === 0) return 0;
 
+  const aFirst = apple > banana;
   let n = 1;
+  let a;
+  let b;
   let max = 0;
-  while (apple >= n || banana >= n || apple + banana >= n) {
-    let a = n;
-    let b = 0;
-    while (a >= 0) {
+
+  while (apple + banana >= n) {
+    a = aFirst ? n : 0;
+    b = aFirst ? 0 : n;
+    while (a >= 0 && b >= 0) {
       if (apple >= a && banana >= b) {
         apple -= a;
         banana -= b;
         max++;
       }
-      a--;
-      b++;
+      generateNextAB();
     }
     n++;
   }
+
   return max;
+
+  // If apple < banana, iteration will be bbb->bba->baa->aaa
+  // If apple >= banana, iteration will be aaa->aab->abb->bbb
+  function generateNextAB() {
+    if (aFirst) {
+      a--;
+      b++;
+    } else {
+      a++;
+      b--;
+    }
+  }
 }
 
 
@@ -35,4 +51,5 @@ console.log(maxMonkeysCanBeFed(4, 5));    // 5
 console.log(maxMonkeysCanBeFed(3, 3));    // 4
 console.log(maxMonkeysCanBeFed(10, 10));  // 9
 console.log(maxMonkeysCanBeFed(0, 10));   // 4
-
+console.log(maxMonkeysCanBeFed(6, 16));   // 9
+console.log(maxMonkeysCanBeFed(16, 6));   // 9
